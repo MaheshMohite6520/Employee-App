3a4e0f80165ae5d198f6939950f241bc9263e44a
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-09-29 15:14:10 +0530

    Add files via upload

---
+++ employee/src/main/java/com/employee/FetchEmployee.java
@@ -0,0 +1,73 @@
+package com.employee;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/fetch-emp")
+public class FetchEmployee extends HttpServlet 
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		try
+		{
+			System.out.println("Loading PostgreSQL driver...");
+			Class.forName("org.postgresql.Driver");
+			System.out.println("PostgreSQL driver loaded...");
+			
+			System.out.println("Establishing connection...");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/qsp", "postgres", "root");
+			System.out.println("Connection established...");
+			
+			System.out.println("Preparing SQL query...");
+			PreparedStatement ps = con.prepareStatement("select * from empproject");
+			System.out.println("SQL query prepared...");
+			
+			System.out.println("Executing SQL query...");
+			ResultSet rs = ps.executeQuery();
+			System.out.println("SQL query executed...");
+			
+			ArrayList<Employee> employees = new ArrayList<>();
+			
+			while(rs.next())
+			{
+				Employee employee = new Employee();
+				
+				employee.setEmployeeId(rs.getInt("eid"));
+				employee.setEname(rs.getString("name"));
+				employee.setEmail(rs.getString("email"));
+				employee.setDept(rs.getString("department"));
+				employee.setContact(rs.getLong("contact"));
+				employee.setSalary(rs.getDouble("salary"));
+				employee.setAddress(rs.getString("address"));
+				employee.setDesignation(rs.getString("designation"));
+				
+				employees.add(employee);
+			}
+			
+			System.out.println("Employees size: " + employees.size());
+			
+			con.close();
+			
+			req.setAttribute("employees", employees);
+			
+			RequestDispatcher rd = req.getRequestDispatcher("viewEmployees.jsp");
+			rd.forward(req, resp);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ employee/src/main/webapp/updateEmployee.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Update Employee</title>
+<link rel="stylesheet" href="updateEmployee.css">
+</head>
+<body>
+	<h1>Update Employee</h1>
+	<form action="UpdateEmployeeServlet">
+	
+		<br> <label for="eid">EmployeeId:</label> <input type="text" id="eid" name="eid"><br><br>
+		<label for="name">Name:</label> <input type="text" id="name" name="name"><br>
+		<br> <label for="email">Email:</label> <input type="email" id="email" name="email"><br>
+		<br> <label for="department">Department:</label> <input type="text" id="department" name="department"><br>
+		<br> <label for="contact">Contact:</label> <input type="text" id="contact" name="contact"><br><br>
+		<label for="salary">Salary:</label> <input type="text" id="salary" name="salary"><br>
+		<br> <label for="address">Address:</label> <input type="text" id="address" name="address"><br>
+		<br> <label for="designation">Designation:</label> <input type="text" id="designation" name="designation"><br> 
+		
+		<br> <input type="submit" value="Update Employee">
+	</form>
+</body>
+</html>
